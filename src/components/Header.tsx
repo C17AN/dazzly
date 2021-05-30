7ad4912ad954be3f694b1a8e937626a5024af387
c17an <kimchanmin1@gmail.com>
2021-05-31 00:53:15 +0900

    feat(Header, Footer) - 헤더, 푸터 컴포넌트 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {}
+
+const Header = (props: Props) => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  height: 80px;
+  background-color: #2766b8;
+  border-bottom: 15px solid #d3e3ec;
+`;
+
+export default Header;
