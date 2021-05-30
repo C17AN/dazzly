ca66339c2328bb5c1c925ba23141bc3a71674fd1
c17an <kimchanmin1@gmail.com>
2021-05-31 00:54:19 +0900

    feat(App.tsx) - App 컴포넌트에 슬롯 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Palette from "./components/Palette";
 import { GlobalStyle } from "./style/global";
+import Slot from "./components/Slot";
+import AddPalette from "./components/AddPalette";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
+  const [paletteList, setPaletteList]: [boolean[], any] = useState([]);
   return (
     <div className="App">
       <GlobalStyle />
-      <Palette />
+      <Header />
+      <Slot>
+        {paletteList.map((palette, index) => (
+          <Palette />
+        ))}
+        <Palette />
+        <Palette />
+        <AddPalette />
+        <AddPalette />
+      </Slot>
+      <Footer />
     </div>
   );
 }
