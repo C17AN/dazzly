10d790427918ddedc07533c984d20c9584e19182
c17an <kimchanmin1@gmail.com>
2021-05-31 13:15:59 +0900

    온라인 폰트 임포트

--- src/style/global.ts
+++ src/style/global.ts
@@ -10,5 +10,11 @@ export const GlobalStyle = createGlobalStyle`
     }
     body {
         font-family: 'NanumSquare';
+        @font-face {
+            font-family: 'KyoboHand';
+            src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.0/KyoboHand.woff') format('woff');
+            font-weight: normal;
+            font-style: normal;
+        }
     }
 `;
