3b9772b88d01e8844dda7713245003b60abac56c
c17an <kimchanmin1@gmail.com>
2021-06-04 02:14:26 +0900

    feat(Home.tsx): 홈 데모 팔레트 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,16 +9,19 @@ const Home = () => {
         <p>여러분의 색감을 아낌없이 어필해 보세요!</p>
         <p>색으로 표현하는 SNS, 대즐리</p>
       </Description>
-      <ImageContainer>
-        <img src={demo1} width={500} />
-        <ImagePaletteContainer>
-          <div className="demo-palette-1-blue" />
-          <div className="demo-palette-1-orange_1" />
-          <div className="demo-palette-1-orange_2" />
-          <div className="demo-palette-1-black" />
-          <div className="demo-palette-1-green" />
-        </ImagePaletteContainer>
-      </ImageContainer>
+      <DemoImageCard>
+        <ImageContainer>
+          <img src={demo1} height={300} />
+          <ImagePaletteContainer>
+            <div className="demo-palette-1-blue" />
+            <div className="demo-palette-1-orange_1" />
+            <div className="demo-palette-1-orange_2" />
+            <div className="demo-palette-1-orange_3" />
+            <div className="demo-palette-1-black" />
+            <div className="demo-palette-1-green" />
+          </ImagePaletteContainer>
+        </ImageContainer>
+      </DemoImageCard>
     </Container>
   );
 };
@@ -39,6 +42,14 @@ const Container = styled.div`
 
 const Description = styled.div``;
 
+const DemoImageCard = styled.div`
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 2rem;
+  background-color: rgba(200, 200, 200, 0.1);
+  border-radius: 12px;
+`;
+
 const ImageContainer = styled.div`
   display: flex;
   img {
@@ -47,24 +58,52 @@ const ImageContainer = styled.div`
 `;
 
 const ImagePaletteContainer = styled.div`
-  width: 100px;
+  width: 150px;
+  height: auto;
+  display: flex;
+  margin-left: 1rem;
+  flex-direction: column;
   .demo-palette-1-blue::after {
     content: " ";
-    display: block;
-    background-color: #190e49;
-    height: 150px;
+    display: flex;
+    background-color: #1d5491;
+    width: 150px;
+    height: 50px;
   }
   .demo-palette-1-orange_1::after {
     content: " ";
-    display: block;
-    background-color: #d88639;
-    height: 150px;
+    display: flex;
+    background-color: #e9ad83;
+    width: 150px;
+    height: 50px;
   }
   .demo-palette-1-orange_2::after {
     content: " ";
-    display: block;
-    background-color: #d6bd4e;
-    height: 150px;
+    display: flex;
+    background-color: #82676a;
+    width: 150px;
+    height: 50px;
+  }
+  .demo-palette-1-orange_3::after {
+    content: " ";
+    display: flex;
+    background-color: #a57466;
+    width: 150px;
+    height: 50px;
+  }
+  .demo-palette-1-black::after {
+    content: " ";
+    display: flex;
+    background-color: #1d1c1b;
+    width: 150px;
+    height: 50px;
+  }
+  .demo-palette-1-green::after {
+    content: " ";
+    display: flex;
+    background-color: #3b520a;
+    width: 150px;
+    height: 50px;
   }
 `;
 
